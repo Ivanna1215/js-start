a4e3a72c99cf55a928297ab6ff025a59f9e35242
Ivanna_1109 <ivanna.kovaliv1215@gmail.com>
2021-11-23 13:41:12 +0300

    commit1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log ('Hello world');
+// console.log ('Hello world');
+// let number = 15;
+// let string = vasya;
+// const Constant ="value cant be changen";
+// number=4
+// console.log (number)
+
+alert('Hello');
+alert (3+5);
+prompt ('What is your name?');
+const Name = prompt ("What is your name?");
